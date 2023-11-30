63355b5f589acdc891cdd83fd4ca49d4d71f1f39
Daria <sypkova@kraud.ru>
2023-11-30 14:00:59 +1000

    fix react pagination styles

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,14 +1,20 @@
 import React, { FC } from 'react'
 import ReactPaginate, { ReactPaginateProps } from "react-paginate";
-import 'react-paginate/theme/basic'
+import styles from './pagination.module.scss'
 
 const Pagination: FC<ReactPaginateProps> = (props) => {
   return (
     <ReactPaginate
-      className="react-paginate"
+      activeClassName={`${styles.item} ${styles.active}`}
+      containerClassName={`${styles.pagination}`}
+      disabledClassName={`${styles['disabled-page']}`}
+      marginPagesDisplayed={2}
+      nextClassName={`${styles.item} ${styles.next}`}
+      pageClassName={`${styles.item} ${styles['pagination-page']}`}
+      previousClassName={`${styles.item} ${styles.previous}`}
       breakLabel="..."
       nextLabel="next >"
-      pageRangeDisplayed={5}
+      pageRangeDisplayed={2}
       previousLabel="< previous"
       renderOnZeroPageCount={null}
       {...props}
